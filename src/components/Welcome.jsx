3f72c8449db1d269973c17df8cfb7f1d7337d5df
Vedansh Saini <vedanshsaini06@gmail.com>
2025-12-03 04:09:53 +0000

    add dock component and integrate  react-tooltip

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -19,7 +19,7 @@ const renderText = (text, className, baseWeight = 400) => {
 };
 
 const setupTextHover = (container, type) => {
-  if (!container) return;
+  if (!container) return () => {};
 
   const letters = container.querySelectorAll("span");
   const { min, max, default: base } = FONT_WEIGHTS[type];
@@ -83,7 +83,7 @@ const Welcome = () => {
       </h1>
 
       <div className="small-screen">
-        <p>This Portfolio is designed for desktop/tabled screens only.</p>
+        <p>This Portfolio is designed for desktop/tablet screens only.</p>
       </div>
     </section>
   );
