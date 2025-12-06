1b9b2225cea86d786124d4027f2e31e9d3e6c238
Vedansh Saini <vedanshsaini06@gmail.com>
2025-12-06 16:42:53 +0000

    add functionality to see image

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
 import { Dock, Navbar, Welcome } from "#components";
-import { Finder, Resume, Safari, Terminal, Text } from "#windows";
+import { Finder, Resume, Safari, Terminal, Text, Image } from "#windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -17,6 +17,7 @@ const App = () => {
       <Resume />
       <Finder />
       <Text />
+      <Image />
     </main>
   );
 };
--- src/windows/index.js
+++ src/windows/index.js
@@ -3,5 +3,6 @@ import Safari from "#windows/Safari.jsx";
 import Resume from "#windows/Resume.jsx";
 import Finder from "#windows/Finder.jsx";
 import Text from "#windows/Text.jsx";
+import Image from "#windows/Image.jsx";
 
-export { Terminal, Safari, Resume, Finder, Text };
+export { Terminal, Safari, Resume, Finder, Text, Image };
